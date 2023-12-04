75a86f53cd960dc3ea35b55180eed97a8499a8fc
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2023-12-04 10:32:01 +0100

    initial commit

---
+++ test.tsx
@@ -0,0 +1,20 @@
+const svg = (
+  <svg height="300" width="400">
+    <defs>
+      <linearGradient id="gr1" x1="0%" y1="60%" x2="100%" y2="0%">
+        <stop offset="5%" style="stop-color:rgb(255,255,3);stop-opacity:1" />
+        <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+      </linearGradient>
+    </defs>
+    <symbol
+      id="b-logo-rounded"
+      width="12"
+      viewBox="0 0 23 23"
+      fill="currentColor"
+    >
+      <path d="M12.36 9.175c.337-.041.597-.319.62-.662V6.122c0-.424-.099-.718-.664-.718h-1.823v3.774h1.87l-.002-.003zm.654 4.252c0-.509-.142-.98-.718-.98h-1.762v4.304h1.856c.18-.003.35-.082.469-.218.12-.137.177-.317.16-.499v-2.63l-.005.023zM.842 2.3v17.48c0 1.15.908 2.3 2.27 2.3h17.25c1.362 0 2.27-1.15 2.27-2.3V2.3c0-1.15-.908-2.3-2.27-2.3H3.111C1.75 0 .842 1.15.842 2.3zm15.422 10.195v5.782c0 .933-.888 1.674-1.519 1.674H7.18V2.207h7.243c1.034.009 1.867.863 1.867 1.913v5.23c0 .66-.9 1.315-1.578 1.464v.04c.714.072 1.545.641 1.554 1.638l-.002.003z" />
+    </symbol>
+    <ellipse cx="125" cy="150" rx="100" ry="60" fill="url(#gr1)" />
+    Sorry but this browser does not support inline SVG.
+  </svg>
+);
